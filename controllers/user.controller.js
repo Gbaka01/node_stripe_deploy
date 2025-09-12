bfe0526fcff3840a426545846b1c7fba1e422abe
gbaka01 <gore.goli@gmail.com>
2025-09-12 22:56:52 +0200

    commitgg1

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -41,7 +41,7 @@ const login = async(req, res) => {
         if(!user){
             return res.status(400).json({message: "invalid credentials"})
         }
-        const isMatch = bcrypt.compare(password, user.password)
+        const isMatch = await bcrypt.compare(password, user.password)
         if(!isMatch){
             return res.status(400).json({message: "invalid invalides"})
         }
