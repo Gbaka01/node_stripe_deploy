c635f6d78c203b552c1ed296aa476ed1574d8329
Gbaka01 <gore.goli@gmail.com>
2026-07-01 00:14:12 +0200

    commitgg538

--- routes/image.route.js
+++ routes/image.route.js
@@ -1,5 +1,6 @@
 
 import { Router } from "express";
+import cloudinary from "../config/cloudinary.js";
 import {
   createImage,
   getAllImages,
@@ -8,9 +9,28 @@ import {
 import { upload } from "../middlewares/multer.js";
 
 const router = Router();
-
 router.post("/new", upload.single("name"), createImage);
 router.get("/all", getAllImages);
 router.delete("/:id", deleteImage);
+
+
+
+
+router.post("/upload", upload.single("image"), async (req, res) => {
+  try {
+    const result = await cloudinary.uploader.upload(req.file.path, {
+      folder: "dessinsgore",
+    });
+
+    res.json({
+      message: "Image uploadée avec succès",
+      url: result.secure_url,
+      public_id: result.public_id,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Erreur upload Cloudinary", error });
+  }
+});
+
 
 export default router;
