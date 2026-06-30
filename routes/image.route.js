5652896ad744e6a71db93923337ed7b1a185dc2d
Gbaka01 <gore.goli@gmail.com>
2026-07-01 01:01:38 +0200

    commitgg542

--- routes/image.route.js
+++ routes/image.route.js
@@ -17,28 +17,18 @@ router.delete("/:id", deleteImage);
 
 
 router.post("/upload", upload.single("name"), async (req, res) => {
-  try {
-    console.log(req.body);
-    console.log(req.file);
+  console.log("BODY :", req.body);
+  console.log("FILE :", req.file);
 
-    if (!req.file) {
-      return res.status(400).json({ message: "Aucun fichier reçu" });
-    }
-
-    // upload Cloudinary ou sauvegarde MongoDB ici
-
-    res.status(201).json({
-      message: "Image enregistrée avec succès",
-      file: req.file,
-      alt: req.body.alt,
-    });
-  } catch (error) {
-    console.error("Erreur serveur upload :", error);
-    res.status(500).json({
-      message: "Erreur interne du serveur",
-      error: error.message,
-    });
+  if (!req.file) {
+    return res.status(400).json({ message: "Aucun fichier reçu" });
   }
+
+  res.json({
+    message: "Image reçue",
+    file: req.file,
+    alt: req.body.alt,
+  });
 });
 
 export default router;
