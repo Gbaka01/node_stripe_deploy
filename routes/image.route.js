a8d87b980d81b5a3903b2d379ddc8a9f093d2b80
Gbaka01 <gore.goli@gmail.com>
2026-07-01 02:10:39 +0200

    commitgg546

--- routes/image.route.js
+++ routes/image.route.js
@@ -1,6 +1,5 @@
 
 import { Router } from "express";
-import cloudinary from "../config/cloudinary.js";
 import {
   createImage,
   getAllImages,
@@ -14,20 +13,5 @@ router.get("/all", getAllImages);
 router.delete("/:id", deleteImage);
 
 
-
-
-router.post("/upload", upload.single("name"), async (req, res) => {
-  console.log("BODY :", req.body);
-  console.log("FILE :", req.file);
-
-  if (!req.file) {
-    return res.status(400).json({ message: "Aucun fichier reçu" });
-  }
-
-  res.json({
-   url: result.secure_url,
-  public_id: result.public_id,
-  });
-});
 
 export default router;
